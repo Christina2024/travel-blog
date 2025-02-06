a9b3016293ca1f4a77843feb430b43bcc459f16d
Christina-2024 <christina_johnson@live.co.uk>
2025-02-06 19:35:19 +0000

    Added text and changed and customised layout.

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -3,20 +3,42 @@ import "./header.css";
 export default function Header() {
   return (
     <div className="header">
-      <div className="headerTitles">
-        <span className="headerTitle">React & Node</span>
-        <span className="headerTitle">Blog</span>
+      <div className="aboutText">
+        <p><em>Hi,  </em> I'm Annita!<br/> <br/>
+          Welcome to my blog where solo <strong>travelling</strong>  and <strong>in colour</strong> meets community!<br />
+          <br/>
+          This community is built upon the interests and <em>some </em>curiousity (as newbies) for solo<br/>
+          travelling as women of colour. <br />
+          <br/>
+          This is a space where I like to express some adventures I have been, with advice of pros <br />
+          and cons for varied destinations. Learning to become unafraid of the unknown and be more self-reliant <br />
+          through solo-travelling has helped me to face my challenge of flying alone. <br />
+          Being in a foreign country not speaking the native lnguage with no one around me has empowered me to<br />
+          overcome self-doubt.
+          Life is a journey and the only way to explore is to get ten toes  <br />
+          in and explore! <br />
+          As well as supporting our fellow sistas on the <br />
+          journeys 
+        </p>
       </div>
-      <img
-        width="350px"
-        src="https://media.istockphoto.com/id/1140379519/photo/african-woman-relaxing-in-swimming-pool.jpg?s=612x612&w=0&k=20&c=HfOJTviKJNzXqNI4zhSGxh32PjE5aOv9Tlu1Vz-S_mk="
-        alt="woman smiling and floating in water"
-      ></img>
-      <img
-        src="https://media.istockphoto.com/id/1156838940/photo/african-american-teen-girl-with-her-feet-in-the-sand.jpg?s=612x612&w=0&k=20&c=OReyHPuqNJ5IV8wlh2Y-gx0puOdxu7oLQkOeWRx-TKM="
-        alt="feet resting on top of sand"
-      ></img>
-      <img src="swimming_pool.jpg" alt="woman_in_pool" />
-    </div>
+      <div className="headerImages">
+        <img
+          src="floating_in_water.jpg"
+          alt="woman smiling and floating in water"
+        ></img>
+        <img src="feet.jpg" alt="feet resting on top of sand"></img>
+        <img
+          className="hotel"
+          src="hotel.jpeg"
+          alt="woman_in_pool"
+        />
+        <img
+          height="500px"
+          width="550px"
+          src="hand.jpg"
+          alt="A hand"
+        ></img>
+       </div>
+     </div>
   );
 }
--- src/topbar/TopBar.jsx
+++ src/topbar/TopBar.jsx
@@ -2,30 +2,27 @@ import "./topBar.css";
 
 export default function TopBar() {
   return (
-    <div className="top">
-      <div className="topLeft">
-        <i class="socialMedia fa-brands fa-square-instagram"></i>
-        <i class="socialMedia fa-brands fa-youtube"></i>
-        <i class="socialMedia fa-brands fa-square-facebook"></i>
+    <>
+      <div className="top">
+        <div className="topCenter">
+          <h1>Travelling in Colour</h1>
+          <ul className="topList">
+            <li className="topListItem">Home</li>
+            <li className="topListItem">Destinations</li>
+            <li className="topListItem">Blog</li>
+            <li className="topListItem">Solo-travel 101</li>
+            <li className="topListItem">Join the tribe</li>
+          </ul>
+        </div>
+        <div className="thumbnailRight">
+          <img
+            className="thumbnailImg"
+            src="https://inclubmagazine.com/wp-content/uploads/2023/09/cover.jpeg"
+            alt="woman standing on cliff"
+          />
+          <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
+        </div>
       </div>
-      <div className="topCenter">
-        <ul className="topList">
-          <li className="topListItem">Home</li>
-          <li className="topListItem">About</li>
-          <li className="topListItem">Destinations</li>
-          <li className="topListItem">Blog</li>
-          <li className="topListItem">Travel tips</li>
-          <li className="topListItem">Community</li>
-        </ul>
-      </div>
-      <div className="topRight">
-        <img
-          className="topImg"
-          src="https://inclubmagazine.com/wp-content/uploads/2023/09/cover.jpeg"
-          alt="woman standing on cliff"
-        />
-        <i className="topSearchIcon fa-solid fa-magnifying-glass"></i>
-      </div>
-    </div>
+    </>
   );
 }
